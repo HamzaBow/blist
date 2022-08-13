4083cb3d87a6ec29eff3df4e216000abda077ba4
Hamza Bow <hamzabow77@gmail.com>
2022-08-14 00:07:42 +0100

    display the image of each book from google books API

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -1,13 +1,15 @@
 import { NextPage } from 'next'
 import React from 'react'
 interface Props {
+  id: string;
   title: string;
   subtitle: string;
   author: string;
 }
-const BookCard:NextPage<Props> = ({ title, subtitle, author }) => {
+const BookCard:NextPage<Props> = ({ id, title, subtitle, author }) => {
   return (
-    <div className="card w-96 bg-base-100 shadow-xl m-4">
+    <div className="card card-side bg-base-100 shadow-xl m-4">
+      <figure><img src={`https://books.google.dz/books/content?id=${id}&printsec=frontcover&img=1&zoom=1`} alt="Album" /></figure>
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
         <p>{subtitle}</p>
--- components/Columns.tsx
+++ components/Columns.tsx
@@ -5,8 +5,8 @@ const Columns = () => {
   return (
     <div>
       {Object.keys(initialData.books).map((key: any) => {
-        const { title, subtitle, author } = initialData.books[key]
-        return <BookCard title={title} subtitle={subtitle} author={author} />
+        const { id, title, subtitle, author } = initialData.books[key]
+        return <BookCard id={id} title={title} subtitle={subtitle} author={author} />
       })}
     </div>
   )
