4ab3889e4bdf739d85197cf5b0e3e27baf16960c
Hamza Bow <hamzabow77@gmail.com>
2022-08-13 19:22:58 +0100

    changed the default footer section

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 const Home: NextPage = () => {
+  const copyrightYear = `${new Date().getFullYear()}`
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
@@ -84,15 +85,11 @@ const Home: NextPage = () => {
       </main>
 
       <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
+        <p
           className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
+          &copy; Copyright{' '} <strong>📘Blist</strong> <span id="copyright-year">{copyrightYear}.</span>
+        </p>
       </footer>
     </div>
   )
