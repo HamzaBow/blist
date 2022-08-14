fc72ad6cefe92939145a48eeff0a216cf432a299
Hamza Bow <hamzabow77@gmail.com>
2022-08-14 01:48:48 +0100

    fix indentation & add empty handler & import statement

--- components/Columns.tsx
+++ components/Columns.tsx
@@ -1,17 +1,21 @@
 import Column from "./Column";
 import initialData from "./initial-data";
+import { DragDropContext } from "react-beautiful-dnd";
 const Columns = () => {
   const readingColumn = initialData.columns.reading;
   const books = Object.keys(initialData.books).map(
     (key: string) => initialData.books[key]
   );
+  const dragEndHandler = (result:any) => {
+
+  }
   return (
-  <div className="bg-indigo-200">
-    <h2 className="font-medium leading-tight text-4xl text-center py-3">{readingColumn.title}</h2>
-    <DragDropContext onDragEnd>
-      <Column key={readingColumn.id} column={readingColumn} books={books} />
-    </DragDropContext>
-  </div>
+    <div className="bg-indigo-200">
+      <h2 className="font-medium leading-tight text-4xl text-center py-3">{readingColumn.title}</h2>
+      <DragDropContext onDragEnd={dragEndHandler}>
+        <Column key={readingColumn.id} column={readingColumn} books={books} />
+      </DragDropContext>
+    </div>
   )
 };
 
