6b29138b145cfa20b9101a3b53b8c4996395c2b3
Hamza Bow <hamzabow77@gmail.com>
2022-08-21 23:00:55 +0100

    use swr package to fetch data & show loading (& error) states

--- components/SearchUI.tsx
+++ components/SearchUI.tsx
@@ -1,25 +1,77 @@
 import React, { useState } from "react";
 import BookCard from "./BookCard";
+import useSWR from "swr";
+import { NextPage } from "next";
+
+const stringifyAuthors = (authors: string[]) => {
+  if (authors.length === 0) {
+    return ""
+  }
+  if (authors.length === 1) {
+    return authors[0]
+  }
+  return authors.slice(0, authors.length - 1).join(", ") + " and " + authors[authors.length - 1]
+}
+
+interface SRDProps {
+  searchQuery: string;
+}
+const SearchResultsDisplay:NextPage<SRDProps> = ({ searchQuery }) => {
+  // @ts-ignore
+  const fetcher = (...args) => fetch(...args).then(res => res.json())
+  const { data, error } = useSWR(`https://www.googleapis.com/books/v1/volumes?q=${searchQuery}&langRestrict=en`, fetcher)
+  console.log('data', data)
+  console.log('error :>> ', error);
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
+  // render data
+  return (
+    <div className="book-search-results">
+      {/* @ts-ignore */}
+      {data.items.map((bookItem: any, index) => {
+        // const { id, title, subtitle, author } = bookItem;
+        const id = bookItem.id
+        const title = bookItem.volumeInfo.title
+        const subtitle = bookItem.volumeInfo.subtitle || ""
+        const authors: string[] = bookItem.volumeInfo.authors || []
+        return (
+        <div key={index} className={"card card-side shadow-xl mx-4 mb-4 bg-base-100"}>
+          <figure className="w-24">
+            <img
+              src={`https://books.google.dz/books/content?id=${id}&printsec=frontcover&img=1&zoom=1`}
+              alt={`${title} Book Cover`}
+              className="w-24 h-36"
+            />
+          </figure>
+          <div className="card-body p-4">
+            <h2 className="card-title">{title}</h2>
+            <p>{subtitle}</p>
+            <div className="card-actions justify-end">
+              <span className="text-slate-500">{stringifyAuthors(authors)}</span>
+            </div>
+          </div>
+      </div>
+
+        )})}
+    </div>
+
+
+  )
+}
 
 const SearchUI = () => {
   const [searchResults, setSearchResults] = useState([])
   const [searchQuery, setSearchQuery] = useState("sapiens")
+  const [searchBtnClicked, setSearchBtnClicked] = useState(false)
 
   const handleBookSearch = async () => {
-    const res = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchQuery}&langRestrict=en`)
-    const data = await res.json()
-    setSearchResults(data.items)
-    console.log(data.items)
-  }
-  const stringifyAuthors = (authors: string[]) => {
-    if (authors.length === 0) {
-      return ""
-    }
-    if (authors.length === 1) {
-      return authors[0]
-    }
-    return authors.slice(0, authors.length - 1).join(", ") + " and " + authors[authors.length - 1]
-
+    // const res = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchQuery}&langRestrict=en`)
+    // const data = await res.json()
+    // setSearchResults(data.items)
+    // console.log(data.items)
+    setSearchBtnClicked(true)
   }
 
   return (
@@ -45,33 +97,9 @@ const SearchUI = () => {
           </button>
         </div>
       </div>
-      <div className="book-search-results">
-        {searchResults.map((bookItem: any, index) => {
-          // const { id, title, subtitle, author } = bookItem;
-          const id = bookItem.id
-          const title = bookItem.volumeInfo.title
-          const subtitle = bookItem.volumeInfo.subtitle || ""
-          const authors: string[] = bookItem.volumeInfo.authors || []
-          return (
-          <div key={index} className={"card card-side shadow-xl mx-4 mb-4 bg-base-100"}>
-            <figure className="w-24">
-              <img
-                src={`https://books.google.dz/books/content?id=${id}&printsec=frontcover&img=1&zoom=1`}
-                alt={`${title} Book Cover`}
-                className="w-24 h-36"
-              />
-            </figure>
-            <div className="card-body p-4">
-              <h2 className="card-title">{title}</h2>
-              <p>{subtitle}</p>
-              <div className="card-actions justify-end">
-                <span className="text-slate-500">{stringifyAuthors(authors)}</span>
-              </div>
-            </div>
-        </div>
-
-          )})}
-      </div>
+      { searchBtnClicked &&
+        <SearchResultsDisplay searchQuery={searchQuery} />
+      }
     </div>
   );
 };
