3f041ee8f875a495a334bbeb59b8984540544af4
Hamza Bow <hamzabow77@gmail.com>
2022-08-21 23:44:03 +0100

    show alert component when fetching fails (search)

--- components/SearchUI.tsx
+++ components/SearchUI.tsx
@@ -26,7 +26,14 @@ const SearchResultsDisplay:NextPage<SRDProps> = ({ searchQuery }) => {
   console.log('data', data)
   console.log('error :>> ', error);
 
-  if (error) return <div>failed to load</div>
+  if (error) return (
+    <div className="alert alert-error shadow-lg mb-2">
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+        <span>Error! Failed to load.</span>
+      </div>
+    </div>
+  )
   if (!data) return <progress className="progress progress-info w-[23.3rem]"></progress>
 
   // render data
