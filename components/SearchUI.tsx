d418327a8b223d3d68acc9f8c836dcc53b6ded81
Hamza Bow <hamzabow77@gmail.com>
2022-08-21 23:51:34 +0100

    fix: show alert & inform user when no books found (search)

--- components/SearchUI.tsx
+++ components/SearchUI.tsx
@@ -36,6 +36,14 @@ const SearchResultsDisplay:NextPage<SRDProps> = ({ searchQuery }) => {
   )
   if (!data) return <progress className="progress progress-info w-[23.3rem]"></progress>
 
+  if (data.totalItems === 0) return (
+    <div className="alert alert-warning shadow-lg mb-2">
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+        <span>Sorry! no books found.</span>
+      </div>
+    </div>
+  )
   // render data
   return (
     <div className="book-search-results">
