2ca6283d3c073d58d633e1574b1c2429bd0b5c2d
Hamza Bow <hamzabow77@gmail.com>
2022-08-22 07:42:02 +0100

    use app name & icon in the center of navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -44,7 +44,7 @@ const Navbar:NextPage<Props> = ({ setSearchUIShow, darkTheme, setDarkTheme }) =>
         </div>
       </div>
       <div className="navbar-center">
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <a className="btn btn-ghost normal-case text-xl">📘 Blist</a>
       </div>
       <div className="navbar-end">
         <button className="btn btn-ghost btn-circle" onClick={() => setSearchUIShow(true)}>
