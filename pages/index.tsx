2b076dd07ac33a149f7900b2adb300f86b38ee5b
Hamza Bow <hamzabow77@gmail.com>
2022-08-22 07:07:25 +0100

    add navbar to the page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import Columns from "../components/Columns";
+import Navbar from "../components/Navbar";
 import SearchUI from "../components/SearchUI";
 
 const Home: NextPage = () => {
@@ -24,6 +25,7 @@ const Home: NextPage = () => {
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📘</text></svg>"
         />
       </Head>
+      <Navbar />
       <main className="mb-8">
         <div className="mb-4">
           <label className="flex gap-4 justify-center">
