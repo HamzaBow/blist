cd84fce528b6bfbdbd47297ab724275814fe5c38
Hamza Bow <hamzabow77@gmail.com>
2022-08-22 07:38:00 +0100

    add toggle dark theme from navbar functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ const Home: NextPage = () => {
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📘</text></svg>"
         />
       </Head>
-      <Navbar setSearchUIShow={setSearchUIShow} />
+      <Navbar setSearchUIShow={setSearchUIShow} darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
       <main className="mb-8">
         <div className="mb-4">
           <label className="flex gap-4 justify-center">
